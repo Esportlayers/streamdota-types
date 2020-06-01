ad3808bab53013f8fa08cfdfd884aa72dd463672
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-06-01 11:57:31 +0200

    Adjusted package struct;

--- Entities/BetOverlay.d.ts
+++ Entities/BetOverlay.d.ts
@@ -1,4 +1,3 @@
-
 export interface BetOverlay {
     userId: number;
     fontFamily: string;
---
+++ Entities/index.d.ts
@@ -0,0 +1,7 @@
+export * from './BetOverlay';
+export * from './BetRound';
+export * from './BetSeason';
+export * from './DotOverlay';
+export * from './Streamer';
+export * from './User';
+export * from './Watcher';
