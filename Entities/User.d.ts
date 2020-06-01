9aa412804dcdfd76b0c7cb962d1b9333f31b89b5
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-06-01 16:28:26 +0200

    Updated; Added missing types;

--- Entities/User.d.ts
+++ Entities/User.d.ts
@@ -1,40 +1,42 @@
 export interface User {
-    id: number;
-    twitchId: number;
-    displayName: string;
-    created: number;
-    avatar: string;
-    avatarWEBP: string;
-    avatarJP2: string;
-    profileUrl: string;
-    gsiAuth: string;
-    dotaStatsFrom: string;
-    useBets: boolean;
-    betSeasonId: number | null;
+	id: number;
+	twitchId: number;
+	displayName: string;
+	created: number;
+	avatar: string;
+	avatarWEBP: string;
+	avatarJP2: string;
+	profileUrl: string;
+	gsiAuth: string;
+	dotaStatsFrom: string;
+	useBets: boolean;
+	betSeasonId: number | null;
+	gsiConnected: string;
+	frameApiKey: string;
 }
 
 export interface SteamConnection {
-    id: number;
-    userId: number;
-    steamId: string;
+	id: number;
+	userId: number;
+	steamId: string;
 }
 
 export interface BotData {
-    useBot: boolean;
-    customBotName: string;
-    customBotToken: string;
-    commandTrigger: string;
+	useBot: boolean;
+	customBotName: string;
+	customBotToken: string;
+	commandTrigger: string;
 }
 
 export interface Command {
-    active: boolean;
-    command: string;
-    message: string;
-    type: 'default' | 'dotaWL' | 'betting_streamer' | 'betting_user';
-    identifier: 'startbet' | 'bet' | 'betwinner';
+	active: boolean;
+	command: string;
+	message: string;
+	type: 'default' | 'dotaWL' | 'betting_streamer' | 'betting_user';
+	identifier: 'startbet' | 'bet' | 'betwinner';
 }
 export interface Timer {
-    period: number;
-    message: string;
-    active: boolean;
+	period: number;
+	message: string;
+	active: boolean;
 }
