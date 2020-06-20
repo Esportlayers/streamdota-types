4bcbb96aa73978d2d67fcd7d381669f0406dbeb5
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-06-20 10:21:15 +0200

    Adjusted command type;

--- Entities/Command.d.ts
+++ Entities/Command.d.ts
@@ -3,7 +3,7 @@ export interface Command {
 	active: boolean;
 	command: string;
 	message: string;
-	type: 'default' | 'dotaWL' | 'betting_streamer' | 'betting_user';
+	type: 'default' | 'dotaWinLoss' | 'betting_streamer' | 'betting_user';
 	noResponse: boolean;
 	deleteAble: boolean;
 	identifier: 'bet' | 'startbet' | 'betwinner';
